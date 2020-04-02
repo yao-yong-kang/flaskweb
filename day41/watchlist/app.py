a434fe52d030744aa1f125315aedd266c4ff15da
yyk <2670211238@qq.com>
2020-04-02 14:22:35 +0800

    静态文件

--- day41/watchlist/app.py
+++ day41/watchlist/app.py
@@ -9,5 +9,9 @@ def index():
         {'title':'奥特曼','year':"2079"},
         {'title':'战狼','year':"2104"},
         {'title':'外星人','year':"9999"},
+        {'title':'泰罗奥特曼','year':"2079"},
+        {'title':'奥特曼','year':"2079"},
+        {'title':'奥特曼','year':"2079"},
+        {'title':'奥特曼','year':"2079"},
     ]
     return render_template('index.html',name=name,movies=movies)
