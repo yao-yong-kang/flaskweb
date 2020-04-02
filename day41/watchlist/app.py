a848d4a11cc588efc8c78546b64615bac75b8946
yyk <2670211238@qq.com>
2020-04-02 11:11:46 +0800

    第一次提交，项目初始化完成

---
+++ day41/watchlist/app.py
@@ -0,0 +1,13 @@
+from flask import Flask,url_for,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    name='yyk'
+    movies=[
+        {'title':'奥特曼','year':"2079"},
+        {'title':'战狼','year':"2104"},
+        {'title':'外星人','year':"9999"},
+    ]
+    return render_template('index.html',name=name,movies=movies)
