3017d8eb3dee81969f81d4f3f06200b0521ed559
yaoyongkang <2670211238@qq.com>
2020-04-16 11:09:14 +0800

    axios&& mockjs

---
+++ simple/mock/mock.js
@@ -0,0 +1,11 @@
+
+import Mock from 'mockjs'
+
+Mock.mock('/getNewsList/',{
+    'list|5':[
+        {
+            url:'@url',
+            title:'@ctitle(5,20)'
+        }
+    ]
+})
--- simple/src/main.js
+++ simple/src/main.js
@@ -1,34 +1,26 @@
 
 import Vue from 'vue'
-import App from './App.vue'
-// import VueRouter from 'vue-router'
-// import Layout from './Layout.vue'
-// import IndexPage from './pages/index.vue'
+import VueRouter from 'vue-router'
+import Layout from './Layout.vue'
+import IndexPage from './pages/index.vue'
+import mock from '../mock/mock.js'
 
-// Vue.use(VueRouter)
-// let router = new VueRouter({
-//   mode:'history',
-//   routes:[
-//     {
-//       path:'/',
-//       component:IndexPage
-//     }
-//   ]
-// })
-
-// new Vue({
-//   el: '#app',
-//   router,
-//   components:{
-//     Layout
-//   },
-//   template: '<Layout/>'
-// })
+Vue.use(VueRouter)
+let router = new VueRouter({
+  mode:'history',
+  routes:[
+    {
+      path:'/',
+      component:IndexPage
+    }
+  ]
+})
 
 new Vue({
-  el:'#app',
-  // render:h=>h(App),
+  el: '#app',
+  router,
   components:{
-    App
-  }
+    Layout
+  },
+  template: '<Layout/>'
 })
