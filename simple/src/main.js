7f39cd77ed222888cc3f04fbba5e817e14039dd7
yaoyongkang <2670211238@qq.com>
2020-04-16 16:14:17 +0800

    slider组件结构和样式

--- simple/src/main.js
+++ simple/src/main.js
@@ -1,9 +1,8 @@
-
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Layout from './Layout.vue'
 import IndexPage from './pages/index.vue'
-import mock from '../mock/mock.js'
+import mock from '../mock/mock'
 
 Vue.use(VueRouter)
 let router = new VueRouter({
